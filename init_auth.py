2a55a52f09ba2b54851d65f0f49ec12b7b6cc936
Mark Demore II <53943861+mdemore2@users.noreply.github.com>
2024-11-10 15:06:50 -0500

    updated init_auth.py to write to .env file and updated readme

--- init_auth.py
+++ init_auth.py
@@ -14,4 +14,10 @@ expiry_time = session.expiry_time
 print("TOKEN_TYPE=", token_type)
 print("ACCESS_TOKEN=", access_token)
 print("REFRESH_TOKEN=", refresh_token)
-print("EXPIRY_TIME:=", expiry_time)
+print("EXPIRY_TIME=", expiry_time)
+
+with open(".env", "w") as f:
+    f.write(f"TOKEN_TYPE={token_type}\n")
+    f.write(f"ACCESS_TOKEN={access_token}\n")
+    f.write(f"REFRESH_TOKEN={refresh_token}\n")
+    f.write(f"EXPIRY_TIME={expiry_time}\n")
